c5def0a83f3e4c356d876d6ca184c604e16912c0
Danil Leiman <danilleqman@gmail.com>
2020-08-19 20:56:03 +0300

    Updates

---
+++ precache-manifest.0dc5cdfa43035cebccfbb302cf3f3415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3222184b556d0ddb4db4683f6bf40ac6",
+    "url": "/nav-bar/index.html"
+  },
+  {
+    "revision": "8732548d5d0efc54779e",
+    "url": "/nav-bar/static/css/main.a7b51115.chunk.css"
+  },
+  {
+    "revision": "b4f7641778b6ce45b6d3",
+    "url": "/nav-bar/static/js/2.623e9d36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nav-bar/static/js/2.623e9d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8732548d5d0efc54779e",
+    "url": "/nav-bar/static/js/main.3eb84195.chunk.js"
+  },
+  {
+    "revision": "4551a5b90ce6d0e6aa18",
+    "url": "/nav-bar/static/js/runtime-main.f8c5f043.js"
+  }
+]);
